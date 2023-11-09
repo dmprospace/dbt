d20b4f4edf31d14264fc46963d5cf3ead3813293
Devesh Mohnani <DMOHNANI@YAHOO.COM>
2023-11-09 22:25:07 +0000

    3rd

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -13,6 +13,24 @@ with source_data as (
 
     select 1 as id
     union all
+    select 2 as id
+    union all
+    select 3 as id
+    union all
+    select 4 as id
+    union all
+    select 5 as id
+    union all
+    select 6 as id
+    union all
+    select 7 as id
+    union all
+    select 8 as id
+    union all
+    select 9 as id
+    union all
+    select 10 as id
+    union all
     select null as id
 
 )
